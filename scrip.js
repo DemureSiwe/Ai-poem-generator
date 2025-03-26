30d1cc763ee7757459bf9788e4b410d12f1941d1
DemureBusi <letsetse.b@gmail.com>
2025-03-27 01:24:34 +0200

    added API intergration

--- scrip.js
+++ scrip.js
@@ -1,12 +1,28 @@
+function displayPoem(response) {
+  console.log("poem generated");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: ["Roses are red"],
-    autoStart: true,
-    delay: 2,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "24ao33ccbd25f045000ce8tbae8037df";
+  let prompt = `User instruction: generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romentic poet and enjoy writing short love poems. Your goal is to follow user instructions using HTML format without mentioning HTML in your poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`promt: ${prompt}`);
+  console.log(`promt: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
